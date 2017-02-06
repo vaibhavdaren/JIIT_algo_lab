cb97f5d2a58b3a80d9a294c90b7b6796f1f8c247
vaibhavdaren <vaibhavdaren@gmail.com>
2017-02-06 14:41:08 +0530

    Create Q1_a_iisi.cpp
    a[i]==i using divide and conquer

---
+++ week6/Q1_a_iisi.cpp
@@ -0,0 +1,18 @@
+int findIndex(int* arr, int size)
+{
+    assert(arr);
+    assert(size > 0);
+    int low = 0;
+    int high = size-1;
+    while (low <= high)
+    {
+        int middle = (low + high)/2;
+        if (arr[middle] == middle)
+            return middle;
+        if (arr[middle] < middle)
+            low = middle + 1;
+        else
+            high = middle - 1;
+    }
+    return -1;
+}
